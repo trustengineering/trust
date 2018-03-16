5ba7a7f4235a455c7341aa020e2c36919cf934a2
Lewis Barclay <lewis.barclay@gmail.com>
2018-03-16 09:28:03 +0000

    Refactor (lambda messenger): Convert Message to class

--- lambda/messenger/src/domain/message.js
+++ lambda/messenger/src/domain/message.js
@@ -4,28 +4,30 @@
 
 const validations = require('./validations');
 
-function Message(rawMessage) {
-  this.rawMessage = rawMessage;
+class Message{
+  constructor(rawMessage){
+    this.rawMessage = rawMessage;
+  }
+
+  validate(){
+    const isValid = rawMessage =>
+      validations.isDefined(rawMessage) &&
+      validations.isDefined(rawMessage.sender) &&
+      validations.isAString(rawMessage.subject) &&
+      validations.isAString(rawMessage.body) &&
+      validations.isAString(rawMessage.sender.email) &&
+      validations.isAString(rawMessage.sender.name);
+
+    if (!isValid(this.rawMessage)) {
+      throw new Error(Message.Errors.invalidRequest);
+    }
+
+    return true;
+  }
 }
 
 Message.Errors = {
   invalidRequest: 'Invalid message request'
-};
-
-Message.prototype.validate = function messageRequestValidate() {
-  const isValid = rawMessage =>
-    validations.isDefined(rawMessage) &&
-    validations.isDefined(rawMessage.sender) &&
-    validations.isAString(rawMessage.subject) &&
-    validations.isAString(rawMessage.body) &&
-    validations.isAString(rawMessage.sender.email) &&
-    validations.isAString(rawMessage.sender.name);
-
-  if (!isValid(this.rawMessage)) {
-    throw new Error(Message.Errors.invalidRequest);
-  }
-
-  return true;
 };
 
 module.exports = Message;
