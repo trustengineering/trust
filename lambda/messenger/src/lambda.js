3cfb76310ed813d726ddd896a2a9250fb36a76c6
Lewis Barclay <lewis.barclay@googlemail.com>
2018-03-23 01:02:51 +0000

    Feature: Implemebt SES in messenger Lambda.

--- lambda/messenger/src/lambda.js
+++ lambda/messenger/src/lambda.js
@@ -3,29 +3,11 @@
  */
 
 import { Messenger } from './domain';
-import LogChannel from './domain/channels/log-channel';
-import Message from "./domain/message";
+import AwsSesChannel from './domain/channels/aws-ses-channel';
+import Message from './domain/message';
 
 const messenger = (event, context, callback) => {
-  Messenger.send(
-    new Message(event.body),
-    new LogChannel(msg => {
-      callback(null, msg.rawMessage);
-    })
-  );
-
-  // const response = {
-  //   statusCode: 200,
-  //   body: JSON.stringify({
-  //     message: 'Go Serverless v1.0! Your function executed successfully!',
-  //     input: event
-  //   })
-  // };
-  //
-  // callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+  Messenger.send(new Message(event.body), new AwsSesChannel());
 };
 
 export default messenger;
